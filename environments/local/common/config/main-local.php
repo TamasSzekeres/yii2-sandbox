8b340591f26937f0a692b484329c3d505fb77278
Szekeres Tamás <szektam2@gmail.com>
2019-05-23 18:43:42 +0200

    Adds & Mods

--- environments/local/common/config/main-local.php
+++ environments/local/common/config/main-local.php
@@ -4,8 +4,8 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=sandbox',
-            'username' => 'szekeres',
-            'password' => 'abcd1234',
+            'username' => 'root',
+            'password' => '',
             'charset' => 'utf8',
         ],
 //        'sqlite' => [
